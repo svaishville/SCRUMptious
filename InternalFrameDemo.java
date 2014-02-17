26e41c2a20f745922246e4ae0faf679c971b99ca
svaishville <svaishville@hotmail.com>
2014-02-17 14:55:23 -0800

    Update InternalFrameDemo.java
    minor updates to menu bar items.

--- InternalFrameDemo.java
+++ InternalFrameDemo.java
@@ -1,4 +1,3 @@
- 
 import javax.swing.JInternalFrame;
 import javax.swing.JDesktopPane;
 import javax.swing.JMenu;
@@ -8,6 +7,18 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.KeyStroke;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+ 
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
  
 import java.awt.event.*;
 import java.awt.*;
@@ -33,7 +44,7 @@ public class InternalFrameDemo extends JFrame
  
         //Set up the GUI.
         desktop = new JDesktopPane(); //a specialized layered pane
-        createFrame(); //create first "window"
+        //createFrame(); //create first "window" //took out so we don't have a starting note
         setContentPane(desktop);
         setJMenuBar(createMenuBar());
  
@@ -43,14 +54,144 @@ public class InternalFrameDemo extends JFrame
  
     protected JMenuBar createMenuBar() {
         JMenuBar menuBar = new JMenuBar();
+    	JMenu menu, submenu;
+    	JMenuItem menuItem;
+    	JRadioButtonMenuItem rbMenuItem;
+    	JCheckBoxMenuItem cbMenuItem;
+
+        
+    	//Build the first menu.
+    	menu = new JMenu("Backlog");
+    	menu.setMnemonic(KeyEvent.VK_A);
+    	menu.getAccessibleContext().setAccessibleDescription(
+            	"The only menu in this program that has menu items");
+    	menuBar.add(menu);
  
+   	 
+  	 
+
+    	submenu = new JMenu("Add New...");
+    	submenu.setMnemonic(KeyEvent.VK_S);
+ 
+    	menuItem = new JMenuItem("Team Member");
+    	menuItem.setAccelerator(KeyStroke.getKeyStroke(
+            	KeyEvent.VK_2, ActionEvent.ALT_MASK));
+    	submenu.add(menuItem);
+ 
+    	menuItem = new JMenuItem("Note");
+    	submenu.add(menuItem);
+    	menu.add(submenu);
+   	 
+  	 
+    	//a group of JMenuItems
+        menuItem = new JMenuItem("New Note");
+        menuItem.setMnemonic(KeyEvent.VK_N);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_N, ActionEvent.ALT_MASK));
+        menuItem.setActionCommand("new");
+        menuItem.addActionListener(this);
+        menu.add(menuItem);
+    	
+    	menuItem = new JMenuItem("New Item",
+                             	KeyEvent.VK_T);
+    	//menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
+    	menuItem.setAccelerator(KeyStroke.getKeyStroke(
+            	KeyEvent.VK_1, ActionEvent.ALT_MASK));
+    	menuItem.getAccessibleContext().setAccessibleDescription(
+            	"This doesn't really do anything");
+    	menuItem.addActionListener(new ActionListener(){
+    		
+    		public void actionPerformed(ActionEvent e)
+    		{
+    			JOptionPane.showMessageDialog(null, "What's up, 370 class?");
+    			new InternalFrameDemo();
+    		}
+    	}
+    	);
+    	menu.add(menuItem);
+ 
+//    	ImageIcon icon = createImageIcon("images/middle.gif");
+    	//menuItem = new JMenuItem("Close", icon);
+    	menuItem.setAccelerator(KeyStroke.getKeyStroke(
+            	KeyEvent.VK_F4, ActionEvent.ALT_MASK));
+    	menuItem.setMnemonic(KeyEvent.VK_B);
+    	menu.add(menuItem);
+ 
+    	//menuItem = new JMenuItem(icon);
+    	menuItem.setMnemonic(KeyEvent.VK_D);
+    	menu.add(menuItem);
+ 
+    	//a group of radio button menu items
+    	menu.addSeparator();
+    	ButtonGroup group = new ButtonGroup();
+ 
+    	rbMenuItem = new JRadioButtonMenuItem("Grid View");
+    	rbMenuItem.setSelected(true);
+    	rbMenuItem.setMnemonic(KeyEvent.VK_R);
+    	group.add(rbMenuItem);
+    	menu.add(rbMenuItem);
+ 
+    	rbMenuItem = new JRadioButtonMenuItem("List View");
+    	rbMenuItem.setMnemonic(KeyEvent.VK_O);
+    	group.add(rbMenuItem);
+    	menu.add(rbMenuItem);
+ 
+    	//a group of check box menu items
+    	menu.addSeparator();
+    	cbMenuItem = new JCheckBoxMenuItem("Show urgent notes on top");
+    	cbMenuItem.setMnemonic(KeyEvent.VK_C);
+    	menu.add(cbMenuItem);
+ 
+    	cbMenuItem = new JCheckBoxMenuItem("Show sprint team name");
+    	cbMenuItem.setMnemonic(KeyEvent.VK_H);
+    	menu.add(cbMenuItem);
+ 
+    	//a submenu
+   	 
+ 
+    	//Build second menu in the menu bar.
+    	menu = new JMenu("Sprints");
+    	menu.setMnemonic(KeyEvent.VK_N);
+    	menu.getAccessibleContext().setAccessibleDescription(
+            	"This menu does nothing");
+    	menuBar.add(menu);
+    	menuItem = new JMenuItem("New Item",
+            	KeyEvent.VK_T);
+//menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
+    	menuItem.setAccelerator(KeyStroke.getKeyStroke(
+   			 KeyEvent.VK_1, ActionEvent.ALT_MASK));
+    	menuItem.getAccessibleContext().setAccessibleDescription(
+   			 "This doesn't really do anything");
+    	menu.add(menuItem);
+   	 
+   	 
+    	//Build second menu in the menu bar.
+    	menu = new JMenu("Team View");
+    	menu.setMnemonic(KeyEvent.VK_N);
+    	menu.getAccessibleContext().setAccessibleDescription(
+            	"This menu does nothing");
+    	menuBar.add(menu);
+    	menuItem = new JMenuItem("New Item",
+            	KeyEvent.VK_T);
+       
+//menuItem.setMnemonic(KeyEvent.VK_T); //used constructor instead
+    	menuItem.setAccelerator(KeyStroke.getKeyStroke(
+   			 KeyEvent.VK_1, ActionEvent.ALT_MASK));
+    	menuItem.getAccessibleContext().setAccessibleDescription(
+   			 "This doesn't really do anything");
+    	
+    	menu.add(menuItem);
+   	      
+        
+        
+        
         //Set up the lone menu.
-        JMenu menu = new JMenu("Document");
+        menu = new JMenu("Document");
         menu.setMnemonic(KeyEvent.VK_D);
         menuBar.add(menu);
  
         //Set up the first menu item.
-        JMenuItem menuItem = new JMenuItem("New");
+        menuItem = new JMenuItem("New");
         menuItem.setMnemonic(KeyEvent.VK_N);
         menuItem.setAccelerator(KeyStroke.getKeyStroke(
                 KeyEvent.VK_N, ActionEvent.ALT_MASK));
@@ -137,4 +278,3 @@ public class InternalFrameDemo extends JFrame
         });
     }
 }
-
